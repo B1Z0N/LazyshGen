082e420ff72ae9adae18f9050145b20c3860934f
Nikolay Fedurko <kolausf@gmail.com>
2021-08-11 01:46:16 +0300

    Initial commit, project setup.

---
+++ Lazysh/LazyshSourceGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Lazysh
+{
+    [Generator] 
+    public class LazyshSourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            
+        }
+    }
+}
