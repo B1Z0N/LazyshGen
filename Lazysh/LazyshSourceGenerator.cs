9b08d450a313bf6f0a825704e7094cb11de18da1
Nikolay Fedurko <kolausf@gmail.com>
2021-08-18 00:00:15 +0300

    Provided global and local factories, exception and much more.

--- Lazysh/LazyshSourceGenerator.cs
+++ Lazysh/LazyshSourceGenerator.cs
@@ -8,43 +8,35 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace Lazysh {
-    
-
-    public class LazyshSyntaxReceiver : ISyntaxReceiver
-    {
-        public HashSet<TypeDeclarationSyntax> TypeDeclarationsWithTypeParamsAttributes { get; } = new();
-
-        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-        {
-            if (syntaxNode is TypeDeclarationSyntax declaration && declaration.TypeParameterList != null)
-            {
-                foreach (var typeParam in declaration.TypeParameterList.Parameters)
-                {
-                    if (typeParam.AttributeLists.Any()) 
-                        TypeDeclarationsWithTypeParamsAttributes.Add(declaration);
-                }
-            }
-        }
-    }
-
+namespace Lazysh
+{
     [Generator]
     public class LazyshSourceGenerator : ISourceGenerator
     {
+        #region Constants
+
+        private const string ItsGeneratedMsg = "// This code is generated with " + nameof(LazyshSourceGenerator) + ", do not modify by hand!";
+        private const string NamespaceName = "LazyshImpl";
+        private const string ReadmeLink = "https://github.com/B1Z0N/LazyshGen/";
+        
+        #endregion
+
+        #region ISourceGenerator implementation 
+
         public void Initialize(GeneratorInitializationContext context)
             => context.RegisterForSyntaxNotifications(() => new LazyshSyntaxReceiver());
 
         public void Execute(GeneratorExecutionContext context)
         {
-            var namespaceName = "LazyshImpl";
-            
             var compilation = context.Compilation;
             var syntaxReceiver = (LazyshSyntaxReceiver) context.SyntaxReceiver;
 
             var lazyshSrc = @$"
+{ItsGeneratedMsg}
+
 using System;
 
-namespace {namespaceName} {{
+namespace {NamespaceName} {{
     [AttributeUsage(validOn: AttributeTargets.GenericParameter)]
     class LazyshAttribute : Attribute
     {{
@@ -56,13 +48,14 @@ namespace {namespaceName} {{
 }}
 ";
             context.AddSource("Lazysh.cs", lazyshSrc);
-            
+
             var options = (CSharpParseOptions) compilation.SyntaxTrees.First().Options;
             var lazyshSyntaxTree = CSharpSyntaxTree.ParseText(lazyshSrc, options);
             compilation = compilation.AddSyntaxTrees(lazyshSyntaxTree);
 
             // SpinWait.SpinUntil(() => Debugger.IsAttached);
-            var targets = new HashSet<ITypeSymbol>();
+            var allTargets = new HashSet<ITypeSymbol>();
+            var factoryToTargets = new Dictionary<ITypeSymbol, List<ITypeSymbol>>();
             foreach (var target in syntaxReceiver.TypeDeclarationsWithTypeParamsAttributes)
             {
                 var semanticModel = compilation.GetSemanticModel(target.SyntaxTree);
@@ -76,21 +69,101 @@ namespace {namespaceName} {{
 
                 if (toBeLazyshed.Any())
                 {
-                    targets.UnionWith(toBeLazyshed);
-                    
+                    allTargets.UnionWith(toBeLazyshed);
+                    factoryToTargets[semanticModel.GetDeclaredSymbol(target)] = toBeLazyshed;
                 }
             }
-            
-            foreach (var targetType in targets)
+
+            foreach (var targetType in allTargets)
             {
                 context.CancellationToken.ThrowIfCancellationRequested();
 
-                var proxySource = GenerateProxy(targetType, namespaceName);
-                context.AddSource($"Lazysh{targetType.Name.Substring(1)}.cs", proxySource);
+                var proxySource = GenerateProxy(targetType, NamespaceName);
+                context.AddSource($"{GetProxyName(targetType)}.cs", proxySource);
             }
+
+            var factorySource = GenerateFactories(factoryToTargets, allTargets, NamespaceName);
+            context.AddSource($"LazyshFactory.cs", factorySource);
         }
+
+        #endregion
         
-        private static string GenerateFactory(ITypeSymbol)
+        #region Generating classes
+        
+        private static string GenerateFactories(
+            Dictionary<ITypeSymbol, List<ITypeSymbol>> factoryToTargets,
+            HashSet<ITypeSymbol> allTargets,
+            string namespaceName)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($@"
+{ItsGeneratedMsg}
+
+using System;
+using System.Collections.Generic;
+
+namespace {namespaceName} {{
+    public static class LazyshFactory
+    {{
+        static Dictionary<Type, object> interfaceToLazy = new();
+
+        static LazyshFactory()
+        {{");
+
+            foreach (var target in allTargets)
+            {
+                var lazyName = GetProxyName(target);
+                var targetName = GetFullQualifiedName(target);
+                sb.Append(@$"
+           interfaceToLazy[typeof({targetName})] = (Func<Func<{targetName}>, {targetName}>) ({lazyName}.Create);");
+            }
+
+            sb.Append($@"
+        }}
+
+        public static T TryGet<T>(Func<T> getter) where T : class // interface to be exact
+        {{
+            interfaceToLazy.TryGetValue(typeof(T), out var getLazy);
+            if (getLazy == null) return null;
+
+            return ((Func<Func<T>, T>)getLazy)(getter);
+        }}
+
+        public static T Get<T>(Func<T> getter) where T : class // interface to be exact
+        {{
+            return TryGet(getter) ?? throw new LazyshArgumentException(
+                $""'{{typeof(T)}}' is not allowed. {GetAllowedFactoryArgumentsMsg(allTargets)}. Please see README for howto: {ReadmeLink}"");
+        }}
+    }}
+
+    public class LazyshArgumentException : ArgumentException 
+    {{
+        public LazyshArgumentException(string msg) :base(msg) {{ }}    
+    }}
+");
+
+            foreach (var factory in factoryToTargets.Keys)
+            {
+                sb.Append($@"
+    public static class {factory.Name}
+    {{
+        static HashSet<Type> ThisFactoryLazies = new HashSet<Type> {{ typeof({String.Join("), typeof(", factoryToTargets[factory].Select(GetFullQualifiedName))}) }};
+         
+        public static T TryGet<T>(Func<T> getter) where T : class // interface to be exact
+            => ThisFactoryLazies.Contains(typeof(T)) ? LazyshFactory.TryGet<T>(getter) : null;
+
+        public static T Get<T>(Func<T> getter) where T : class // interface to be exact
+        {{
+            return TryGet(getter) ?? throw new LazyshArgumentException(
+                $""'{{typeof(T)}}' is not allowed. {GetAllowedFactoryArgumentsMsg(factoryToTargets[factory])}. Please see README for howto: {ReadmeLink}"");
+        }}
+    }}
+}}");
+            }
+            
+            return sb.ToString();
+        }
 
         private static string GenerateProxy(ITypeSymbol targetType, string namespaceName)
         {
@@ -99,20 +172,21 @@ namespace {namespaceName} {{
                 .ToList();
 
             var fullQualifiedName = GetFullQualifiedName(targetType);
-            
+
             var sb = new StringBuilder();
-            var proxyName = $"Lazysh{targetType.Name.Substring(1)}";
+            var proxyName = GetProxyName(targetType);
             sb.Append($@"
+{ItsGeneratedMsg}
+
 using System;
 
 namespace {namespaceName} {{
-    // generated via LazyshSourceGenerator
     internal class {proxyName} : Lazy<{fullQualifiedName}>, {fullQualifiedName}, ILazysh
     {{
         public {proxyName}(Func<{fullQualifiedName}> val) :base(val)
         {{
         }}");
-            
+
             foreach (var interfaceMethod in allInterfaceMethods)
             {
                 var containingType = interfaceMethod.ContainingType;
@@ -130,6 +204,10 @@ namespace {namespaceName} {{
   }}
 }}").ToString();
         }
+        
+        #endregion
+
+        #region Helpers 
 
         private static string GetFullQualifiedName(ISymbol symbol)
         {
@@ -138,6 +216,30 @@ namespace {namespaceName} {{
                 return containingNamespace.ToDisplayString() + "." + symbol.Name;
 
             return symbol.Name;
+        }
+
+        private static string GetProxyName(ITypeSymbol target) => $"Lazysh{target.Name.Substring(1)}";
+
+        private static string GetAllowedFactoryArgumentsMsg(IEnumerable<ITypeSymbol> allowed)
+            => $"Allowed list: [{String.Join(", ", allowed.Select(GetFullQualifiedName))}]";
+
+        #endregion
+    }
+
+    public class LazyshSyntaxReceiver : ISyntaxReceiver
+    {
+        public HashSet<TypeDeclarationSyntax> TypeDeclarationsWithTypeParamsAttributes { get; } = new();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is TypeDeclarationSyntax declaration && declaration.TypeParameterList != null)
+            {
+                foreach (var typeParam in declaration.TypeParameterList.Parameters)
+                {
+                    if (typeParam.AttributeLists.Any())
+                        TypeDeclarationsWithTypeParamsAttributes.Add(declaration);
+                }
+            }
         }
     }
 }
